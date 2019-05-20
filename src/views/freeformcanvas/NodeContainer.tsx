610396fbed6a9c7ed59e7300344dd770ea145f9a
Mohammad Amoush <muhammedamoush@gmail.com>
2019-05-20 19:47:16 -0400

    Resizing Refactored

--- src/views/freeformcanvas/NodeContainer.tsx
+++ src/views/freeformcanvas/NodeContainer.tsx
@@ -6,20 +6,63 @@ import { TextNodeView } from "../nodes/TextNodeView";
 import { VideoNodeView } from "../nodes/VideoNodeView";
 import "./FreeFormCanvas.scss";
 import React = require("react");
+import { NodeStore } from "../../stores/NodeStore";
 
 interface IProps {
     store: NodeCollectionStore
 }
 
+export enum Resizer_Type {
+    BOTTOM_RIGHT = 0,
+    BOTTOM_LEFT = 1,
+    TOP_RIGHT = 2,
+    TOP_LEFT = 3
+}
+
+
+
 @observer
 export class NodeContainer extends React.Component<IProps> {
+
+    resizeNode = (e: PointerEvent, isPointerDown: boolean, clickedResizer: Resizer_Type, nodeStore: NodeStore): void => {
+        e.stopPropagation();
+        e.preventDefault();
+        if (!isPointerDown) {
+            return;
+        }
+        switch(clickedResizer) {
+            case Resizer_Type.BOTTOM_RIGHT:
+                nodeStore.Width += e.movementX;
+                nodeStore.Height += e.movementY;
+                break;
+            case Resizer_Type.BOTTOM_LEFT:
+                nodeStore.Width -= e.movementX;
+                nodeStore.Height += e.movementY;
+                nodeStore.X += e.movementX;
+                break; 
+            case Resizer_Type.TOP_LEFT:
+                nodeStore.Width -= e.movementX;
+                nodeStore.Height -= e.movementY;
+                nodeStore.Y += e.movementY;
+                nodeStore.X += e.movementX;
+                break; 
+            case Resizer_Type.TOP_RIGHT:
+                nodeStore.Width += e.movementX;
+                nodeStore.Height -= e.movementY;
+                nodeStore.Y += e.movementY;
+                break;       
+        }
+        
+    
+    }
+    
 
     render() {
         return (
             <div className="node-container">
                 {this.props.store.Nodes.map(nodeStore => {
                     if (nodeStore instanceof StaticTextNodeStore) {
-                        return (<TextNodeView key={nodeStore.Id} store={nodeStore as StaticTextNodeStore} />)
+                        return (<TextNodeView key={nodeStore.Id} store={nodeStore as StaticTextNodeStore} resize={this.resizeNode} />)
                     } else if (nodeStore instanceof VideoNodeStore) {
                         return (<VideoNodeView key={nodeStore.Id} store={nodeStore as VideoNodeStore} />)
                     }
--- src/views/nodes/TextNodeView.tsx
+++ src/views/nodes/TextNodeView.tsx
@@ -4,16 +4,14 @@ import "./NodeView.scss";
 import { TopBar } from "./TopBar";
 import React = require("react");
 import { action, observable } from "mobx";
+import { Resizer_Type } from "../freeformcanvas/NodeContainer";
+import { NodeStore } from "../../stores/NodeStore";
 
 interface IProps {
     store: StaticTextNodeStore;
+    resize: (e:PointerEvent, pointerFlag: boolean, clickedResizer: Resizer_Type, nodeStore: NodeStore) => void;
 }
-enum Resizer_Type {
-    BOTTOM_RIGHT = 0,
-    BOTTOM_LEFT = 1,
-    TOP_RIGHT = 2,
-    TOP_LEFT = 3
-}
+
 
 @observer
 export class TextNodeView extends React.Component<IProps> {
@@ -26,56 +24,25 @@ export class TextNodeView extends React.Component<IProps> {
         e.stopPropagation();
         e.preventDefault();
         this._isPointerDown = true;
-        document.removeEventListener("pointermove", this.onPointerMove);
-        document.addEventListener("pointermove", this.onPointerMove);
+        document.removeEventListener("pointermove", this.resizeEvent);
+        document.addEventListener("pointermove", this.resizeEvent);
         document.removeEventListener("pointerup", this.onPointerUp);
         document.addEventListener("pointerup", this.onPointerUp);
+    }
+
+    resizeEvent = (e: PointerEvent) => {
+        this.props.resize(e, this._isPointerDown, this.clickedResizer, this.props.store);
     }
 
     onPointerUp = (e: PointerEvent): void => {
         e.stopPropagation();
         e.preventDefault();
         this._isPointerDown = false;
-        document.removeEventListener("pointermove", this.onPointerMove);
+        document.removeEventListener("pointermove", this.resizeEvent);
         document.removeEventListener("pointerup", this.onPointerUp);
     }
 
-    onPointerMove = (e: PointerEvent): void => {
-        e.stopPropagation();
-        e.preventDefault();
-        if (!this._isPointerDown) {
-            return;
-        }
-        if(this.clickedResizer === Resizer_Type.BOTTOM_RIGHT) {
-            
-            console.log(e.movementX);
-            console.log("width" + this.props.store.Width);
-        } 
-        switch(this.clickedResizer) {
-            case Resizer_Type.BOTTOM_RIGHT:
-                this.props.store.Width += e.movementX;
-                this.props.store.Height += e.movementY;
-                break;
-            case Resizer_Type.BOTTOM_LEFT:
-                this.props.store.Width -= e.movementX;
-                this.props.store.Height += e.movementY;
-                this.props.store.X += e.movementX;
-                break; 
-            case Resizer_Type.TOP_LEFT:
-                this.props.store.Width -= e.movementX;
-                this.props.store.Height -= e.movementY;
-                this.props.store.Y += e.movementY;
-                this.props.store.X += e.movementX;
-                break; 
-            case Resizer_Type.TOP_RIGHT:
-                this.props.store.Width += e.movementX;
-                this.props.store.Height -= e.movementY;
-                this.props.store.Y += e.movementY;
-                break;       
-        }
-        
-
-    }
+    
     render() {
         let store = this.props.store;
         return (
